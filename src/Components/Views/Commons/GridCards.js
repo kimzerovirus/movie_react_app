3e12946593c93f566b86dcd77c6a868ae196229f
kzv <>
2021-02-18 22:34:58 +0900

    2021-02-18

--- src/Components/Views/Commons/GridCards.js
+++ src/Components/Views/Commons/GridCards.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function GridCards(props) {
     if (props.image !== null) {
         return (
             <li className="grid-card">
-                <a href={`${process.env.PUBLIC_URL}/movie/${props.movieId}`} >
-
+                {/* <a href={`${process.env.PUBLIC_URL}/movie/${props.movieId}`} > */}
+                <Link to={`/movie/${props.movieId}`}>
                     <img className="grid-img-size" src={props.image} alt={props.movieName} />
-                </a>
+                </Link>
+                {/* </a> */}
             </li>
         )
     } else {
--- src/Components/Views/Commons/SearchIcon.js
+++ src/Components/Views/Commons/SearchIcon.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function SearchIcon() {
     return (
         <div className="search-icon">
-            <a href={`${process.env.PUBLIC_URL}/search`}>
+            {/* <a href={`${process.env.PUBLIC_URL}`}> */}
+            <Link to="/search">
                 <img src={`${process.env.PUBLIC_URL}/search_icon.svg`} alt="" />
-            </a>
+            </Link>
+            {/* </a> */}
         </div>
     )
 }
