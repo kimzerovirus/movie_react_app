54fe6722147b68518d2b6840416829c7f8d008b1
kimzerovirus <yg9699k@naver.com>
2022-01-18 14:50:04 +0900

    add style

--- src/components/MainPage/MainPage.style.ts
+++ src/components/MainPage/MainPage.style.ts
@@ -7,6 +7,12 @@ export const Wrapper = styled.div`
 export const GridList = styled.ul`
 	display: flex;
 	flex-wrap: wrap;
-	gap: 1em;
-	justify-content: flex-start;
+	/* display: grid;
+	grid-template-columns: repeat(auto-fill, 1fr); */
+	/* @media all and (max-width: 1179px) {
+		width: calc(100% / 4 - 0.75rem); // 3rem % 4
+	}
+	@media all and (max-width: 767px) {
+		width: calc(100% / 2 - 0.5rem); // 총gap%한줄당컬럼수 = 1rem % 2
+	} */
 `;
